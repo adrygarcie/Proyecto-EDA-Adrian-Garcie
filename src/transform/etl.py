5ae181808952ba02f5b4c0d263ac43f32d719713
adrygarcie <adrygarcie@gmail.com>
2025-04-04 15:36:24 +0200

    Añadir script etl completo y csv limpio

---
+++ src/transform/etl.py
@@ -0,0 +1,41 @@
+#Importamos las librerias necesarias para el etl.
+import pandas as pd
+import os
+from pathlib import Path
+
+#Cargamos el archivo csv en un dataframe de pandas.
+
+import pandas as pd
+
+# Obtener la ruta del archivo .py
+current_dir = Path(__file__).resolve().parent
+
+# Construir la ruta relativa al archivo CSV
+csv_path = current_dir.parents[1] / 'data' / 'raw' / 'scraped_earthquakes.csv'
+
+# Leer el archivo CSV
+df = pd.read_csv(csv_path)
+
+
+#convertimos la columna Date & Time UTC a un objeto datetime.
+df['Date & Time UTC'] = pd.to_datetime(df['Date & Time UTC'], format='%Y-%m-%d %H:%M:%S.%f', errors='coerce')
+
+
+# Intentamos dividir la columna 'Region' por la coma y el espacio
+split_columns = df['Region'].str.split(', ', expand=True)
+
+# Verificamos si la división fue exitosa
+if 1 in split_columns.columns:
+    df['Country'] = split_columns[1]
+else:
+    df['Country'] = split_columns[0]  # Si no hubo división, mantenemos el valor original en 'Country'
+
+df['Region'] = split_columns[0].fillna(df['Region'])  # Si no hubo división, mantenemos el valor original en 'Region'
+
+#Eliminamos columnas innecesarias.
+clean_df = df.drop(columns=['Type', 'Network', 'A/M'])
+
+#Guardamos el dataframe limpio en un archivo csv.
+clean_csv_path = current_dir.parents[1] / 'data' / 'processed' / 'clean_earthquakes.csv'
+clean_df.to_csv(clean_csv_path, index=False)
+print("Data cleaned and saved to:", clean_csv_path)
